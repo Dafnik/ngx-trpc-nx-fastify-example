1226d8682f2ad8569bcbbda75b6076d83f25d675
Dafnik <git@dafnik.me>
2023-03-28 12:08:44 +0200

    Initial commit

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poweruptime-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
